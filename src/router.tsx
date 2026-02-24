7f8d2696e2856c435a6be862eb7ccb157535c65b
yazavala_meli <yamil.zavala@mercadolibre.com>
2026-02-24 14:22:14 -0300

    Standardize router indentation and replace 404 route with wildcard catch-all

--- src/router.tsx
+++ src/router.tsx
@@ -5,74 +5,65 @@ import HandleLayout from './layouts/HandleLayout';
 import HandleView from './views/HandleView';
 import HomeView from './views/HomeView';
 import LinkTreeView from './views/LinkTreeView';
-import LoginView from './views/LoginView'
+import LoginView from './views/LoginView';
 import NotFoundView from './views/NotFoundView';
 import ProfileView from './views/ProfileView';
-import RegisterView from './views/RegisterView'
+import RegisterView from './views/RegisterView';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <HomeView />,
-        errorElement: <Error />,
-    },
-    {
-        path: '/auth/login',
-        element: <LoginView />,
-        errorElement: <Error />,
-    },
-    {
-        path: '/auth/register',
-        element: <RegisterView />,
-        errorElement: <Error />,
-    },
-    {
-        path: '/admin',
-        element: <AppLayout />,
-        errorElement: <Error />,
-        children: [
-            {
-                index: true,
-                element: <LinkTreeView />,
-                errorElement: <ErrorElement />,
-            },
-            {
-                path: 'profile',
-                element: <ProfileView />,
-                errorElement: <ErrorElement />,
-            }
-        ]
-    },
-    {
-        path: '/:handle',
-        element: <HandleLayout />,
-        errorElement: <Error />,
-        children: [
-            {
-                index: true,
-                element: <HandleView />,
-                errorElement: <ErrorElement />,
-            },
-        ]
-    },
-    {
-        path: '/404',
-        element: <HandleLayout />,
-        children: [
-            {
-                index: true,
-                element: <NotFoundView />,
-            },
-        ]
-    },
-
-])
+  {
+    path: '/',
+    element: <HomeView />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/auth/login',
+    element: <LoginView />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/auth/register',
+    element: <RegisterView />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/admin',
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <LinkTreeView />,
+        errorElement: <ErrorElement />,
+      },
+      {
+        path: 'profile',
+        element: <ProfileView />,
+        errorElement: <ErrorElement />,
+      },
+    ],
+  },
+  {
+    path: '/:handle',
+    element: <HandleLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <HandleView />,
+        errorElement: <ErrorElement />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFoundView />,
+  },
+]);
 
 const WrapperRouter = () => {
-    return (
-        <RouterProvider router={router} />
-    );
+  return <RouterProvider router={router} />;
 };
 
 export default WrapperRouter;
--- src/views/HandleView.tsx
+++ src/views/HandleView.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query"
 import { getUserByHandle } from "../api/DevTree"
 import Loading from "../components/Loading";
-import { Navigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import HandleData from "../components/HandleData";
+import NotFoundView from "./NotFoundView";
 
 
 const HandleView = () => {
@@ -17,7 +18,7 @@ const HandleView = () => {
 
     if (isLoading) return (<Loading />);
 
-    if(error) return (<Navigate to={'/404'} />)
+    if(error) return (<NotFoundView/>)
 
     if(data) return <HandleData data={data} />
 }
